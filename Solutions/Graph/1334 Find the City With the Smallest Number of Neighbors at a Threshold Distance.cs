e2ca9d1fdc6b33d4929e08ddcbc8fba81a76a7f8
anshuman <liku1997@gmail.com>
2022-06-24 15:00:59 +0530

    new

---
+++ Solutions/Graph/1334 Find the City With the Smallest Number of Neighbors at a Threshold Distance.cs
@@ -0,0 +1,55 @@
+﻿//Return the city with the smallest number of cities that are reachable through some path and whose distance is at most distanceThreshold,
+//If there are multiple such cities, return the city with the greatest number.
+
+public class Solution1134
+{
+    public int FindTheCity(int n, int[][] edges, int t)
+    {
+        int[][] dist = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            dist[i] = new int[n];
+            Array.Fill(dist[i], 100000000);//not int.MaxValue because it overflows
+        }
+        for (int i = 0; i < n; i++)
+        {
+            dist[i][i] = 0;
+        }
+        foreach (var item in edges)
+        {
+            dist[item[0]][item[1]] = item[2];
+            dist[item[1]][item[0]] = item[2];
+        }
+
+        //floyd warshall, all source all destination shortest distance
+        for (int k = 0; k < n; k++)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    dist[i][j] = Math.Min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+        
+        int res = 0, smallest = int.MaxValue;
+        for (int i = 0; i < n; i++)
+        {
+            int c = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if (dist[i][j] <= t)
+                {
+                    c++;
+                }
+            }
+            if (c <= smallest)
+            {
+                res = i;
+                smallest = c;
+            }
+        }
+        return res;
+    }
+}
---
+++ Solutions/String/13 Roman to Integer.cs
@@ -0,0 +1,31 @@
+﻿//roman to int
+public class Solution13
+{
+    public int RomanToInt(string s)
+    {
+        Dictionary<char, int> map = new Dictionary<char, int>();
+        map.Add('I', 1);
+        map.Add('V', 5);
+        map.Add('X', 10);
+        map.Add('L', 50);
+        map.Add('C', 100);
+        map.Add('D', 500);
+        map.Add('M', 1000);
+
+        int res = map[s[s.Length - 1]];
+
+        for (int i = s.Length - 2; i >= 0; i--)
+        {
+            if (map[s[i]] < map[s[i + 1]])
+            {
+                res -= map[s[i]];
+            }
+            else
+            {
+                res += map[s[i]];
+            }
+        }
+
+        return res;
+    }
+}
