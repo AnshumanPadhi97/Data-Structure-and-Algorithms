1daa4f17ed93ec8a7983164e6069eea0f607d77c
Anshuman Padhi <liku1997@gmail.com>
2022-06-26 11:35:32 +0530

    Time: 93 ms (14.48%), Space: 28.5 MB (59.92%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public bool IsPalindrome(int x) {
+        if(x<0)return false;
+        int temp = x;
+        int op = 0;
+        while(temp>0){
+            var t = temp%10;
+            temp/=10;
+            op = op*10+t;
+        }
+        return op==x;
+    }
+}
