b83e00191d00cc001a525019ef2ce633e5492581
Anshuman Padhi <liku1997@gmail.com>
2022-06-27 16:18:49 +0530

    Time: 98 ms (79.77%), Space: 39.9 MB (5.40%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cs
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cs
@@ -2,24 +2,12 @@ public class Solution {
     public ListNode MergeTwoLists(ListNode a, ListNode b) {
         if(a==null)return b;
         if(b==null)return a;
-        var temp = new ListNode(0);
-        var h = temp;
-        while(a!=null && b!=null){
-            if(a.val>b.val){
-                temp.next=b;
-                b=b.next;
-            }else{
-                temp.next=a;
-                a=a.next;
-            }
-            temp=temp.next;
+        if(a.val<=b.val){
+            a.next=MergeTwoLists(a.next,b);
+            return a;
+        }else{
+            b.next=MergeTwoLists(a,b.next);
+            return b;
         }
-        if(a!=null){
-            temp.next=a;
-        }
-        if(b!=null){
-            temp.next=b;
-        }
-        return h.next;
     }
 }
