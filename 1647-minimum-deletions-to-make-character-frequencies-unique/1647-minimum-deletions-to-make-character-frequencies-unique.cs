4cbb2368e47a8e06c698644bae88503f5f9205e9
Anshuman Padhi <liku1997@gmail.com>
2022-06-28 18:25:48 +0530

    Time: 105 ms (80.30%), Space: 41.2 MB (34.33%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cs
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cs
@@ -1,20 +1,31 @@
 public class Solution {
     public int MinDeletions(string s) {
-        int[] a = new int[26];
-        foreach (var item in s)
-        {
-            a[item - 'a']++;
-        }
-        int res = 0;
-        var h = new HashSet<int>();
-        for (int i = 0; i < 26; i++)
-        {
-            while(a[i] > 0 && h.Contains(a[i]))
+            int res = 0;
+            int[] a = new int[26];
+            foreach (var item in s)
             {
-                a[i]--;res++;
+                a[item - 'a']++;
             }
-            h.Add(a[i]);
-        }
-        return res;
+            var pq = new PriorityQueue<int, int>(Comparer<int>.Create((a,b)=>b.CompareTo(a)));
+            foreach (var item in a)
+            {
+                if (item > 0)
+                {
+                    pq.Enqueue(item, item);
+                }
+            }
+            while (pq.Count > 1)
+            {
+                int top = pq.Dequeue();
+                if(top == pq.Peek())
+                {
+                    if (top - 1 > 0)
+                    {
+                        pq.Enqueue(top - 1, top - 1);
+                    }
+                    res++;
+                }
+            }
+            return res;
     }
 }
