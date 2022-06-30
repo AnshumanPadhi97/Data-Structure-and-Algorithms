db00afb6d36aa5bee088564610b2b52c00cfacbc
Anshuman Padhi <liku1997@gmail.com>
2022-06-30 09:18:29 +0530

    Time: 117 ms (48.77%), Space: 37.1 MB (21.42%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    int[] dp = new int[500];
+    public int Rob(int[] nums) {
+        Array.Fill(dp,-1);
+        return h(nums,0);
+    }
+    public int h(int[] nums,int i){
+        if(i>=nums.Length)return 0;
+        if(dp[i]!=-1)return dp[i];
+        return dp[i]=Math.Max(h(nums,i+1),nums[i]+h(nums,i+2));
+    }
+}
