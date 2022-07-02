fec65d47cf1f162ff9f9430e753b2467789674a5
Anshuman Padhi <liku1997@gmail.com>
2022-07-02 11:02:53 +0530

    Time: 280 ms (63.63%), Space: 44 MB (56.53%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public int LadderLength(string beginWord, string endWord, IList<string> wordList) {
+        
+        var res=1;
+        var q = new Queue<string>();
+        var h = wordList.ToHashSet<string>();
+        var vis = new HashSet<string>();
+        
+        q.Enqueue(beginWord);
+        
+        while(q.Count>0){
+            var l = q.Count;
+            while(l-->0){
+                var a = q.Dequeue();   
+                if(a==endWord)return res;
+                
+                for(int j=0;j<a.Length;j++){
+                    for(int k='a';k<='z';k++){
+                        var b=a.ToCharArray();
+                        b[j]=(char)k;
+                        
+                        var s = new string(b);
+                        if(h.Contains(s) && !vis.Contains(s)){
+                            q.Enqueue(s);
+                            vis.Add(s);
+                        }
+                    }
+                }
+            }
+            res+=1;
+        }
+        
+        return 0;
+    }
+}
