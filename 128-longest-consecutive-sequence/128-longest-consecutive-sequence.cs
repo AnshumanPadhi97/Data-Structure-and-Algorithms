ae265ba5c560c2176d7957ae07e2773ba905d21e
Anshuman Padhi <liku1997@gmail.com>
2022-07-05 15:04:36 +0530

    Time: 245 ms (58.64%), Space: 44 MB (93.96%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cs
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cs
@@ -1,19 +1,23 @@
 public class Solution {
     public int LongestConsecutive(int[] nums) {
-        Array.Sort(nums);
-        int l = nums.Length;
-        if(l==0)return 0;
-        int res=1,curr=1;
-        for(int i=1;i<l;i++){
-            if(nums[i]!=nums[i-1]){
-                if(nums[i]==nums[i-1]+1){
-                    curr+=1;
-                }else{
-                    res=Math.Max(res,curr);
-                    curr=1;
+            Array.Sort(nums);
+            if (nums.Length == 0) return 0;
+            int res = 1,curr=1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[i - 1])
+                {
+                    if (nums[i] == nums[i-1]+1)
+                    {
+                        curr++;
+                    }
+                    else
+                    {
+                        res = Math.Max(res, curr);
+                        curr = 1;
+                    }
                 }
             }
-        }
-        return Math.Max(res,curr);
+            return Math.Max(res,curr);
     }
 }
