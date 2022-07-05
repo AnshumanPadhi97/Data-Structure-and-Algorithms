545e788ae621f7d67a4b2fc6765a7d1647a63d25
Anshuman Padhi <liku1997@gmail.com>
2022-07-05 09:26:31 +0530

    Time: 103 ms (93.04%), Space: 42.8 MB (5.70%) - LeetHub

--- 179-largest-number/179-largest-number.cs
+++ 179-largest-number/179-largest-number.cs
@@ -1,29 +1,13 @@
 public class Solution {
     public string LargestNumber(int[] nums) {
-        
-        var arr = new string[nums.Length];
-        
-        for(int i = 0; i < nums.Length; i++){
-            arr[i] = nums[i].ToString();
-        }
-        
-        Comparer<string> comparer = Comparer<string>.Default;
-        Array.Sort<string>(arr, (x, y) =>
-        {
-            string str1 = x + y;
-            string str2 = y + x;
-            return comparer.Compare(str1, str2);
-        });
-        
-        if(arr[nums.Length-1] == "0")
-            return "0";
-        
-        
-        StringBuilder sb = new StringBuilder();
-        for(int i = nums.Length-1; i >= 0; i--){
-            sb.Append(arr[i]);
-        }
-        
-        return sb.ToString();
+            var ls = nums.Select(x => x.ToString()).ToList();
+            ls.Sort((a, b) => (b + a).CompareTo((a + b)));
+            string s = "";
+            if (ls.First() == "0") return "0";
+            for (int i = 0; i < ls.Count; i++)
+            {
+                s += ls[i];
+            }
+            return s;
     }
 }
