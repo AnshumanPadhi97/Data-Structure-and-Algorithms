8c3a13b387ed64a62f3358d58622396ea9a31640
Anshuman Padhi <liku1997@gmail.com>
2022-07-06 09:11:53 +0530

    Time: 196 ms (46.90%), Space: 38.2 MB (43.46%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int LengthOfLIS(int[] nums) {
+            int l = nums.Length;
+            int[] dp = new int[l];
+            Array.Fill(dp, 1);
+            for (int i = 1; i < l; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[j] < nums[i])
+                    {
+                        dp[i] = Math.Max(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+            return dp.Max();
+    }
+}
