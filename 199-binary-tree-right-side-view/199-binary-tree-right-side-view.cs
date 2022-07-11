b31ddd70c3494dd75dc21f196a80699275852af2
Anshuman Padhi <liku1997@gmail.com>
2022-07-11 14:23:38 +0530

    Time: 198 ms (49.62%), Space: 41.4 MB (62.63%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cs
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cs
@@ -1,14 +1,13 @@
 public class Solution {
-    List<int> res =new List<int>();
+    List<int> res = new List<int>();
     public IList<int> RightSideView(TreeNode root) {
         h(root,0);
         return res;
     }
     public void h(TreeNode root,int l){
         if(root==null)return;
-        if(res.Count==l){
+        if(res.Count==l)
             res.Add(root.val);
-        }
         h(root.right,l+1);
         h(root.left,l+1);
     }
