feb0adc5bc18cbd0180ff30fab2eb89921ad1ada
Anshuman Padhi <liku1997@gmail.com>
2022-07-11 11:25:46 +0530

    Time: 130 ms (39.15%), Space: 36.9 MB (68.22%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cs
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cs
@@ -1,14 +1,16 @@
 public class Solution {
     public int LengthOfLongestSubstring(string s) {
-        var d = new Dictionary<char,int>();
+        string t ="";
         int i=0,j=0,res=0,l=s.Length;
         while(j<l){
-            if(d.ContainsKey(s[j])){
-                i=Math.Max(i,d[s[j]]);
-                d.Remove(s[j]);
+            if(!t.Contains(s[j])){
+                t+=s[j];
+                res=Math.Max(res,t.Length);
+            }else{
+                var ind = t.IndexOf(s[j]);
+                t=t.Substring(ind+1);
+                t+=s[j];
             }
-            res=Math.Max(res,j-i+1);
-            d.Add(s[j],j+1);
             j++;
         }
         return res;
