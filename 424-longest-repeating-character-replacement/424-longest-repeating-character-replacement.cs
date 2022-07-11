06a68ba04dfcfac188aad43c6b73b82ec16ad2ba
Anshuman Padhi <liku1997@gmail.com>
2022-07-11 14:29:33 +0530

    Time: 114 ms (56.01%), Space: 35.8 MB (90.78%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cs
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cs
@@ -1,16 +1,14 @@
 public class Solution {
     public int CharacterReplacement(string s, int k) {
-            var f = new int[26];
-            int i = 0, j = 0, res = 0, curr = 0, l = s.Length;
-            while (j < l)
-            {
-                curr = Math.Max(curr, ++f[s[j++] - 'A']);
-                while (j - i - curr > k)
-                {
-                    f[s[i++] - 'A']--;
-                }
-                res = Math.Max(res, j - i);
+        int res=0,i=0,j=0,l=s.Length,curr=0;
+        var a = new int[26];
+        while(j<l){
+            curr = Math.Max(curr,++a[s[j++]-'A']);
+            while(j-i-curr>k){
+                a[s[i++]-'A']--;
             }
-            return res;
+            res=Math.Max(res,j-i);
+        }
+        return res;
     }
 }
