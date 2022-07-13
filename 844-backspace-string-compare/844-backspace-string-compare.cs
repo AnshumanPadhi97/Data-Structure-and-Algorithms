177777a3b7ae97986efd6bad479478d76fd5fafc
Anshuman Padhi <liku1997@gmail.com>
2022-07-13 16:06:22 +0530

    Time: 88 ms (75.07%), Space: 37.1 MB (45.47%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cs
+++ 844-backspace-string-compare/844-backspace-string-compare.cs
@@ -1,31 +1,25 @@
 public class Solution {
     public bool BackspaceCompare(string s, string t) {
-        var st = new Stack<char>();
-        for(int i=0;i<s.Length;i++){
-            if(st.Count>0 && s[i]=='#'){
-                st.Pop();
-            }
-            if(s[i]!='#'){
-                st.Push(s[i]);
-            }
-        }
-        string p ="";
-        while(st.Count>0){
-            p+=st.Pop();
-        }
-        st = new Stack<char>();
-        for(int i=0;i<t.Length;i++){
-            if(st.Count>0 && t[i]=='#'){
-                st.Pop();
-            }
-            if(t[i]!='#'){
-                st.Push(t[i]);
+        return h(s).Equals(h(t));
+    }
+    public string h(string s){
+        int j=s.Length-1;
+        int c=0;
+        string res="";
+        while(j>=0){
+            if(s[j]=='#'){
+                c++;
+                j--;
+            }else{
+                if(c>0){
+                    c--;
+                    j--;
+                }else{
+                    res+=s[j];
+                    j--;
+                }
             }
         }
-        string q ="";
-        while(st.Count>0){
-            q+=st.Pop();
-        }
-        return p.Equals(q);
+        return res;
     }
 }
