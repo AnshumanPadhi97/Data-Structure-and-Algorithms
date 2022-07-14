52a0952da40f97c3d408b36c3ed518f342246119
Anshuman Padhi <liku1997@gmail.com>
2022-07-14 18:14:00 +0530

    Time: 180 ms (46.16%), Space: 44.2 MB (75.85%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cs
+++ 200-number-of-islands/200-number-of-islands.cs
@@ -1,24 +1,24 @@
 public class Solution {
     public int NumIslands(char[][] grid) {
-        int c=0;
-        for(int i=0;i<grid.Length;i++){
-            for(int j=0;j<grid[0].Length;j++){
+        int m = grid.Length;
+        int n = grid[0].Length;
+        int res=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
-                    c++;
-                    dfs(grid,i,j);
+                   res++;
+                    dfs(i,j,grid);
                 }
             }
         }
-        return c;
+        return res;
     }
-    public void dfs(char[][] grid,int i,int j){
-        if(i<0||j<0||i>=grid.Length||j>=grid[0].Length||grid[i][j]=='0'){
-            return;
-        }
+    public void dfs(int i,int j,char[][] grid){
+        if(i<0||j<0||i>=grid.Length||j>=grid[0].Length||grid[i][j]=='0')return;
         grid[i][j]='0';
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
+        dfs(i+1,j,grid);
+        dfs(i-1,j,grid);
+        dfs(i,j+1,grid);
+        dfs(i,j-1,grid);
     }
 }
