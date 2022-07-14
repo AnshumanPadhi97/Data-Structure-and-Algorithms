8766068fc37d713cbaf2c318bfefa1c121d71722
Anshuman Padhi <liku1997@gmail.com>
2022-07-14 09:33:19 +0530

    Time: 57 ms (33.30%), Space: 27 MB (87.63%) - LeetHub

--- 202-happy-number/202-happy-number.cs
+++ 202-happy-number/202-happy-number.cs
@@ -1,17 +1,25 @@
 public class Solution {
-    HashSet<int> h = new HashSet<int>();
     public bool IsHappy(int n) {
-        if(n==1)return true;
-        var s=0;
+        
+        var s=n;
+        var f=n;
+        
+        do{
+            s=h(s);
+            f=h(h(f));
+        }while(s!=f);
+        
+        return s==1;
+    }
+    public int h(int n){
+        var s = 0;
+        
         while(n>0){
             var a = n%10;
             s+=a*a;
             n/=10;
         }
-        if(h.Contains(s)){
-            return false;
-        }
-        h.Add(s);
-        return IsHappy(s);
+        
+        return s;
     }
 }
