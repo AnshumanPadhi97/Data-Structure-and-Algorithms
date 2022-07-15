74cbacb6e8c3bbf021c777b0b61ba23884c06c1b
Anshuman Padhi <liku1997@gmail.com>
2022-07-15 11:04:42 +0530

    Time: 141 ms (49.73%), Space: 38.8 MB (82.40%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cs
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cs
@@ -1,30 +1,25 @@
 public class Solution {
-    Dictionary<int,int> d = new Dictionary<int,int>();
-    int pid=0;
-    
-    public TreeNode BuildTree(int[] a, int[] b) {
-        
-        for(int i=0;i<b.Length;i++){
-            if(!d.ContainsKey(b[i])){
-                d.Add(b[i],-1);
-            }
-            d[b[i]]=i;
-        }
-        
-        return build(a,0,b.Length-1);
+    public TreeNode BuildTree(int[] preorder, int[] inorder) {
+        return h(preorder,inorder,0,preorder.Length-1,0,inorder.Length-1);
     }
-    
-    public TreeNode build(int[] a,int lid,int rid){
-        
-        if(lid>rid){
+    public TreeNode h(int[] p,int[] ino,int pi,int pe,int ii,int ie){
+        if(pi>pe){
             return null;
         }
+        TreeNode root = new TreeNode(p[pi]);
+        int ind = 0;
+        for(int i=ii;i<=ie;i++){
+            if(ino[i]==root.val){
+                ind=i;
+                break;
+            }
+        }
         
-        var root = new TreeNode(a[pid++]);
+        var numsLeft = ind-ii;//left of ind in inorder array
         
-        root.left = build(a,lid,d[root.val]-1);
+        root.left = h(p,ino,pi+1,pi+numsLeft,ii,ind-1);
         
-        root.right = build(a,d[root.val]+1,rid);
+        root.right = h(p,ino,pi+numsLeft+1,pe,ind+1,ie);
         
         return root;
     }
