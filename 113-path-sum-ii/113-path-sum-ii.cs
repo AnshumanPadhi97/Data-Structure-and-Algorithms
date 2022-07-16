89e14285203556b89daeebf8fe6fa5cb6a2be6a9
Anshuman Padhi <liku1997@gmail.com>
2022-07-16 05:57:08 +0530

    Time: 167 ms (80.20%), Space: 44.3 MB (39.85%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cs
+++ 113-path-sum-ii/113-path-sum-ii.cs
@@ -1,30 +1,22 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 public class Solution {
     IList<IList<int>> res = new List<IList<int>>();
-    public IList<IList<int>> PathSum(TreeNode root, int t) {
-        dfs(root,t,new List<int>());
+    public IList<IList<int>> PathSum(TreeNode root, int targetSum) {
+        dfs(root,targetSum,new List<int>());
         return res;
     }
-    public void dfs(TreeNode root, int t, List<int> temp){
+    public void dfs(TreeNode root,int t,List<int> temp){
         if(root==null)return;
+        
         temp.Add(root.val);
-        if(root.left == null && root.right == null && temp.Sum() == t){
-            res.Add(temp.ToList()); 
+        
+        if(root.val==t && root.left==null&&root.right==null)
+        {
+            res.Add(temp.ToList());
         }
-        dfs(root.left,t,temp);
-        dfs(root.right,t,temp);
+        
+        dfs(root.left,t-root.val,temp);
+        dfs(root.right,t-root.val,temp);
+        
         temp.RemoveAt(temp.Count-1);
     }
 }
