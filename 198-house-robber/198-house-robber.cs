361e1fe96e5ba11988deeb8d0c748a464c518e0d
Anshuman Padhi <liku1997@gmail.com>
2022-07-21 14:49:16 +0530

    Time: 183 ms (5.00%), Space: 36.6 MB (73.42%) - LeetHub

--- 198-house-robber/198-house-robber.cs
+++ 198-house-robber/198-house-robber.cs
@@ -1,12 +1,13 @@
 public class Solution {
-    int[] dp = new int[500];
     public int Rob(int[] nums) {
+        int n = nums.Length;
+        int[] dp = new int[n];
         Array.Fill(dp,-1);
-        return h(nums,0);
+        return check(nums,0,dp,n);
     }
-    public int h(int[] nums,int i){
-        if(i>=nums.Length)return 0;
+    public int check(int[] nums, int i, int[] dp, int l){
+        if(i>=l)return 0;
         if(dp[i]!=-1)return dp[i];
-        return dp[i]=Math.Max(h(nums,i+1),nums[i]+h(nums,i+2));
+        return dp[i] = Math.Max(nums[i]+check(nums,i+2,dp,l),check(nums,i+1,dp,l));
     }
 }
