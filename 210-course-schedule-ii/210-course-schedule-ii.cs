1790bd0485f8787ead46675cce3bf540cfaf144d
Anshuman Padhi <liku1997@gmail.com>
2022-07-22 13:24:33 +0530

    Time: 206 ms (67.73%), Space: 45.9 MB (77.55%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cs
+++ 210-course-schedule-ii/210-course-schedule-ii.cs
@@ -1,37 +1,42 @@
 public class Solution {
-    public int[] FindOrder(int n, int[][] p) {
-        var g = new List<List<int>>();
-        var ind = new int[n];
-        var res = new List<int>();
-        var q = new Queue<int>();
-        var h = new HashSet<int>();
-        for(int i=0;i<n;i++){
-            g.Add(new List<int>());
-        }
-        foreach(var item in p){
-            g[item[1]].Add(item[0]);
-            ind[item[0]]++;
-        }
-        for(int i=0;i<n;i++){
-            if(ind[i]==0)
-                q.Enqueue(i);
-        }
-        while(q.Count>0){
-            var u = q.Dequeue();
-            if(h.Contains(u)){
-                return new int[0];
-            }else{
-                h.Add(u);
-                res.Add(u);
+    public int[] FindOrder(int numCourses, int[][] prerequisites) {
+            var h = new HashSet<int>();
+            var q = new Queue<int>();
+            var g = new List<List<int>>();
+            var res = new List<int>();
+            int[] indegree = new int[numCourses];
+            for (int i = 0; i < numCourses; i++)
+            {
+                g.Add(new List<int>());
             }
-            foreach(var item in g[u]){
-                ind[item]-=1;
-                if(ind[item]==0){
-                    q.Enqueue(item);
+            for (int i = 0; i < prerequisites.Length; i++)
+            {
+                g[prerequisites[i][1]].Add(prerequisites[i][0]);
+                indegree[prerequisites[i][0]]++;
+            }
+            for (int i = 0; i < indegree.Length; i++)
+            {
+                if (indegree[i] == 0) q.Enqueue(i);
+            }
+            while (q.Count > 0)
+            {
+                var a = q.Dequeue();
+                if (h.Contains(a))
+                {
+                    return new int[0];
+                }
+                res.Add(a);
+                h.Add(a);
+                foreach (var item in g[a])
+                {
+                    indegree[item]--;
+                    if (indegree[item] == 0)
+                    {
+                        q.Enqueue(item);
+                    }
                 }
             }
-        }
-        if(h.Count!=n)return new int[0];
-        return res.ToArray();
+            if (h.Count != numCourses) return new int[0];
+            return res.ToArray();
     }
 }
