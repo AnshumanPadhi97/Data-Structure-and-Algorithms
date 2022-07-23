2c0c1be35bb983ecf1e8d91a22218c061c3227a9
anshuman <liku1997@gmail.com>
2022-07-23 22:04:44 +0530

    cpp first

--- 00-CP Template/CPTemplate.cs
+++ 00-CP Template/CPTemplate.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 public class Program
 {
-	private static void Test()
+	public void Test()
 	{
 		IEnumerable<int> nM = Console.ReadLine().Split().Select(x => int.Parse(x));
 		int l = nM.ElementAt(0);
@@ -18,8 +18,7 @@ public class Program
 		int T = int.Parse(Console.ReadLine());
 		for (int test_no = 1; test_no < T + 1; test_no++)
 		{
-			Console.Write(String.Format("Case #{0}: ", test_no));
-			Program.Test();
+			//Test();
 		}
 	}
 }
--- Solution.cs
+++ Solution.cs
@@ -10,15 +10,34 @@ using System.Text.RegularExpressions;
 
 namespace basicDS6
 {
-    class st
+    public class st
     {
         private int MOD = 1000000007;
-        
-        
+        public void Test()
+        {
+            //int n = int.Parse(Console.ReadLine());
+            //var s = Console.ReadLine();
+            var nums = Console.ReadLine().Split(' ').Select(ti => int.Parse(ti)).ToList();
+            
+        }
+        //int gcd(int a, int b)
+        //{
+        //    if (a == 0)
+        //        return b;
+
+        //    return gcd(b % a, a);
+        //}
+
         static void Main()
         {
             st p = new st();
-
+            //int T = int.Parse(Console.ReadLine());
+            //for (int test_no = 1; test_no < T + 1; test_no++)
+            //{
+            //    p.Test();
+            //}
+            Console.WriteLine("a");
         }
+
     }
 }
