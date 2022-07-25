583e7aaf8ae10be0bf086ef166404f6bb855a3ef
Anshuman Padhi <liku1997@gmail.com>
2022-07-25 09:25:33 +0530

    Time: 197 ms (59.33%), Space: 43.5 MB (49.29%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cs
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cs
@@ -1,53 +1,27 @@
 public class Solution {
-        public int[] SearchRange(int[] nums, int target)
+    public int[] SearchRange(int[] nums, int target)
+    {
+        var res = new int[] { -1, -1 };
+        var p1 = pb(nums, target-1);
+        var p2 = pb(nums, target);
+        if (p1 == p2) return res;
+        return new int[] { p1, p2 - 1 };
+    }
+    int pb(int[] nums, int t)
+    {
+        int lo = 0, hi = nums.Length;
+        while (lo<hi)
         {
-            var res = new int[] { -1, -1 };
-            res[0] = find1(nums, target);
-            res[1] = find2(nums, target);
-            return res;
-        }
-        int find1(int[] nums,int target)
-        {
-            var res = -1;
-            int lo = 0, hi = nums.Length - 1;
-            while (lo<=hi)
+            var mid = (hi + lo) >> 1;
+            if(nums[mid]> t)
             {
-                var mid = (lo + hi) >> 1;
-                if(nums[mid] >= target)
-                {
-                    hi = mid - 1;
-                }
-                else
-                {
-                    lo = mid + 1;
-                }
-                if (nums[mid]==target)
-                {
-                    res = mid;
-                }
+                hi = mid;
             }
-            return res;
-        }
-        int find2(int[] nums, int target)
-        {
-            var res = -1;
-            int lo = 0, hi = nums.Length - 1;
-            while (lo <= hi)
+            else
             {
-                var mid = (lo + hi) >> 1;
-                if (nums[mid] <= target)
-                {
-                    lo = mid + 1;
-                }
-                else
-                {
-                    hi = mid - 1;
-                }
-                if (nums[mid]==target)
-                {
-                    res = mid;
-                }
+                lo = mid + 1;
             }
-            return res;
         }
+        return lo;
+    }
 }
