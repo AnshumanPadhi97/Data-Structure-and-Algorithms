1de09400018e222747deedbb5004883d96115441
Anshuman Padhi <liku1997@gmail.com>
2022-07-28 15:04:44 +0530

    Time: 12 ms (65.55%), Space: 7.2 MB (77.97%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int a[26]={0},b[26]={0};
+        int a[26]={0};
         for(int i=0;i<s.length();i++)a[s[i]-'a']++;
-        for(int i=0;i<t.length();i++)b[t[i]-'a']++;
+        for(int i=0;i<t.length();i++)a[t[i]-'a']--;
         
         for(int i=0;i<26;i++){
-            if(a[i]!=b[i])return false;
+            if(a[i]!=0)return false;
         }
         return true;
     }
