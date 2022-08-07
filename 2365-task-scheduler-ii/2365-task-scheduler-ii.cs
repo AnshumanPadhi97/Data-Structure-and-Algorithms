6b54bfdee1f13d12ee0ed5cda85187e6b7dc77c4
Anshuman Padhi <liku1997@gmail.com>
2022-08-07 12:29:04 +0530

    Time: 410 ms (100.00%), Space: 58.3 MB (100.00%) - LeetHub

--- 2365-task-scheduler-ii/2365-task-scheduler-ii.cs
+++ 2365-task-scheduler-ii/2365-task-scheduler-ii.cs
@@ -1,18 +1,18 @@
 public class Solution {
     public long TaskSchedulerII(int[] tasks, int space) {
         long day = 0;
-        Dictionary<int, long> lastindex = new Dictionary<int, long>();
+        var d = new Dictionary<int, long>();
         for (int i = 0; i < tasks.Length; i++)
         {
             day++;
-            if (lastindex.ContainsKey(tasks[i]))
+            if (d.ContainsKey(tasks[i]))
             {
-                day = Math.Max(lastindex[tasks[i]] + space + 1, day);
-                lastindex[tasks[i]] = day;
+                day = Math.Max(d[tasks[i]] + space + 1, day);
+                d[tasks[i]] = day;
             }
             else
             {
-                lastindex.Add(tasks[i], day);
+                d.Add(tasks[i], day);
             }
         }
         return day;
