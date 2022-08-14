a3306b5f926c346e1772e8aa9109ab35501a0dc6
Anshuman Padhi <liku1997@gmail.com>
2022-08-14 16:35:39 +0530

    Time: 665 ms (20.00%), Space: 5.9 MB (65.00%) - LeetHub

---
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool check(string& s,string& t){
+        for (int i = 1; i < t.size(); i++)
+        {
+            if(t[i]>t[i-1] && s[i-1]!='I')return false;
+            if(t[i]<t[i-1] && s[i-1]!='D')return false;
+        }
+        return true;
+    }
+
+string smallestNumber(string s) {
+        string t ="";
+        string res="";
+        for (int i = 1; i <= s.size()+1; i++)
+        {
+            t+=to_string(i);
+        }
+        do
+        {
+            if(check(s,t)){
+                if(res.size()==0)res=t;
+                else res = min(res,t);
+            }
+        } while (next_permutation(t.begin(),t.end()));
+        return res;
+    }
+};
