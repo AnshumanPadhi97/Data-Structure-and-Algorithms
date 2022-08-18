ca4415f9629572f2741a94c47b7f55584c60b393
Anshuman Padhi <liku1997@gmail.com>
2022-08-18 16:40:37 +0530

    Time: 275 ms, Memory: 41.5 MB - LeetHub

---
+++ find-the-celebrity/find-the-celebrity.cs
@@ -0,0 +1,35 @@
+public class Solution : Relation {
+        int a = 0;
+        public int FindCelebrity(int n)
+        {
+            a = n;
+            int res = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (Knows(res, i)){
+                    res = i;
+                }
+            }
+            if (h(res))
+            {
+                return res;
+            }
+            return -1;
+        }
+        public bool h(int i)
+        {
+            //if celebrity know anyone then false
+            //if anyone doesnt know celebrity then false
+            //else correct celebrity
+            
+            for (int j = 0; j < a; j++)
+            {
+                if (i == j) continue;
+                if(Knows(i,j) || !Knows(j, i))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+}
