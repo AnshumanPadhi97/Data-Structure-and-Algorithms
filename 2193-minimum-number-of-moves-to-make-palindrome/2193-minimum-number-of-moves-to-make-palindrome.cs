fa1a8a473c1e9b0fa1fba5e74640500a9a3bc43d
Anshuman Padhi <liku1997@gmail.com>
2022-08-19 12:52:25 +0530

    Time: 125 ms (37.25%), Space: 36.5 MB (35.29%) - LeetHub

--- 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.cs
+++ 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.cs
@@ -3,15 +3,15 @@ public class Solution {
             int res = 0;
             while (s.Length > 0)
             {
-                int i = s.IndexOf(s[s.Length - 1]);
-                if (i == s.Length-1)
+                var ind = s.IndexOf(s[s.Length - 1]);
+                if (ind == s.Length - 1)
                 {
-                    res += i / 2;
+                    res += ind / 2;
                 }
                 else
                 {
-                    res += i;
-                    s = s.Substring(0, i) + s.Substring(i + 1);
+                    res += ind;
+                    s = s.Substring(0, ind) + s.Substring(ind + 1);
                 }
                 s = s.Substring(0, s.Length - 1);
             }
