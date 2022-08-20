470371a552fb24273de908b62e329c1069293284
anshuman <liku1997@gmail.com>
2022-08-20 07:38:21 +0530

    oa prep

--- Solution.cs
+++ Solution.cs
@@ -21,31 +21,7 @@ namespace basicDS6
         }
         public int check(int[] a, int[] b)
         {
-            //1 3 2 4
-            //4 2 4
-            var sum1 = a.Sum();
-            var sum2 = b.Sum();
-            if (sum1 != sum2) return -1;
 
-            int i = 0, j = 0, res = 0;
-            while (i < a.Length)
-            {
-                res++;
-                var suma = a[i++];
-                var sumb = b[j++];
-                while (suma != sumb)
-                {
-                    if (suma < sumb)
-                    {
-                        suma += a[i++];
-                    }
-                    else
-                    {
-                        sumb += b[j++];
-                    }
-                }
-            }
-            return res;
         }
     }
 }
