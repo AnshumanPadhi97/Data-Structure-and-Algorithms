9603f887245f9ec523e899272445c4c5cd1a3a24
Anshuman Padhi <liku1997@gmail.com>
2022-08-27 15:03:15 +0530

    Time: 382 ms (10.54%), Space: 36.5 MB (99.80%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>> g(n+1);
-        for (auto &&i : times)
-        {
-            g[i[0]].push_back({i[1],i[2]});
-        }
-
         vector<int> dist(n+1,INT_MAX);
-
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> q;
         dist[k]=0;
-
-        q.push({0,k});
-
-        while (!q.empty())
+    
+        for (int i = 1; i < n; i++)
         {
-            int u = q.top().second;
-            q.pop();
-
-            for (auto &&i : g[u])
+            for (auto &&i : times)
             {
-                if(dist[i.first]>dist[u]+i.second){
-                    dist[i.first]=dist[u]+i.second;
-                    q.push({dist[i.first],i.first});
-                }       
+                int u = i[0], v = i[1], d = i[2];
+                if(dist[u] != INT_MAX && dist[v]>dist[u]+d){
+                    dist[v]=dist[u]+d;
+                }
             }        
-        }
-        int res =*max_element(dist.begin()+1,dist.end());
-        return res == INT_MAX ? -1 : res;
+        }    
+
+        int res=*max_element(dist.begin()+1,dist.end());
+        return res==INT_MAX?-1:res;
     }
 };
