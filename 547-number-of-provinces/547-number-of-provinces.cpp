ace26fac33a80951c26a874c34eea6780a028b9f
Anshuman Padhi <liku1997@gmail.com>
2022-09-09 12:22:38 +0530

    Time: 61 ms (13.06%), Space: 13.8 MB (49.31%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,27 +1,53 @@
+class UnionFind{
+    public :
+        int components;
+        vector<int> rank;
+        vector<int> parent;
+        UnionFind(int n){
+            rank = vector<int>(n,1);
+            parent = vector<int>(n);
+            components = n;
+            for (int i = 0; i < n; i++)
+            {
+                parent[i]=i;
+            }
+        }
+        int find(int p){
+            if(p==parent[p]){
+                return p;
+            }else{
+                return parent[p] = find(parent[p]);
+            }
+        }
+        bool unionn(int u,int v){
+            u = find(u);
+            v = find(v);
+            if(u==v) return false;
+            if(rank[u]>rank[v]){
+                parent[v] = u;
+                rank[u]+=rank[v];
+            }else{
+                parent[u]=v;
+                rank[v]+=rank[u];
+            }
+            components-=1;
+            return true;
+        }
+};
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid,int i){
-        for (int j = 0; j < grid.size(); j++)
-        {
-            if(grid[i][j]){
-                grid[i][j]=grid[j][i]=0;
-                dfs(grid,j);
-            }
-        }
-    }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        int res=0;
-        for (int i = 0; i < n; i++)
+        UnionFind u(n);
+        for (int i = 0; i < n-1; i++)
         {
-            for (int j = 0; j < n; j++)
+            for (int j = i+1; j < n; j++)
             {
                 if(isConnected[i][j]){
-                    dfs(isConnected,i);
-                    res++;
+                    u.unionn(i,j);
                 }
-            }
+            }            
         }
-        return res;
+        return u.components;
     }
 };
