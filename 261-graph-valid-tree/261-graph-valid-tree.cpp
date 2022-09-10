f3686bb5b1941f5f7b496426add2605e743a1a5c
Anshuman Padhi <liku1997@gmail.com>
2022-09-10 07:53:07 +0530

    Time: 40 ms (25.59%), Space: 12 MB (79.03%) - LeetHub

---
+++ 261-graph-valid-tree/261-graph-valid-tree.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+typedef unsigned long long ull;
+typedef long double lld;
+#define all(v) v.begin(), v.end()
+#define revall(v) v.rbegin(), v.rend()
+#define setbits __builtin_popcountll
+#define nline "\n"
+#define MOD1 (int)998244353
+#define INF64 (ll)1e18
+#define INF32 (int)1e9+7
+#define yes cout<<"YES"
+#define no cout<<"NO"
+
+class UnionFind{
+    public :
+        int components;
+        vector<int> rank;
+        vector<int> parent;
+        UnionFind(int n){
+            rank = vector<int>(n,1);
+            parent = vector<int>(n);
+            components = n;
+            for (int i = 0; i < n; i++)
+            {
+                parent[i]=i;
+            }
+        }
+        int find(int p){
+            if(p==parent[p]){
+                return p;
+            }else{
+                return parent[p] = find(parent[p]);
+            }
+        }
+        bool unionn(int u,int v){
+            u = find(u);
+            v = find(v);
+            if(u==v) return false;
+            if(rank[u]>rank[v]){
+                parent[v] = u;
+                rank[u]+=rank[v];
+            }else{
+                parent[u]=v;
+                rank[v]+=rank[u];
+            }
+            components-=1;
+            return true;
+        }
+};
+
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if(edges.size()!=n-1)return false;
+        UnionFind u(n);
+        for (auto &&i : edges)
+        {
+            if(!u.unionn(i[0],i[1])){
+                return false;
+            }   
+        }        
+        return true;
+    }
+};
