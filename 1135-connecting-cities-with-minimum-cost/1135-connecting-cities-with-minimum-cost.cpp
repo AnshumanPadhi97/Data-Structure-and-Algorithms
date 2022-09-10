475ae4cc09748b34f6b3268ef509b366c298a57e
Anshuman Padhi <liku1997@gmail.com>
2022-09-10 07:22:33 +0530

    Time: 432 ms (33.18%), Space: 53.4 MB (45.09%) - LeetHub

--- 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.cpp
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.cpp
@@ -1,72 +1,37 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-#define ll long long
-typedef unsigned long long ull;
-typedef long double lld;
-#define all(v) v.begin(), v.end()
-#define revall(v) v.rbegin(), v.rend()
-#define setbits __builtin_popcountll
-#define nline "\n"
-#define MOD1 (int)998244353
-#define INF64 (ll)1e18
-#define INF32 (int)1e9+7
-#define yes cout<<"YES"
-#define no cout<<"NO"
-
-class UnionFind{
-    public :
-        int components;
-        vector<int> rank;
-        vector<int> parent;
-        UnionFind(int n){
-            rank = vector<int>(n,1);
-            parent = vector<int>(n);
-            components = n;
-            for (int i = 0; i < n; i++)
-            {
-                parent[i]=i;
-            }
-        }
-        int find(int p){
-            if(p==parent[p]){
-                return p;
-            }else{
-                return parent[p] = find(parent[p]);
-            }
-        }
-        bool unionn(int u,int v){
-            u = find(u);
-            v = find(v);
-            if(u==v) return false;
-            if(rank[u]>rank[v]){
-                parent[v] = u;
-                rank[u]+=rank[v];
-            }else{
-                parent[u]=v;
-                rank[v]+=rank[u];
-            }
-            components-=1;
-            return true;
-        }
-};
-
 class Solution {
 public:
-    static bool comp(const vector<int>& a, const vector<int>& b) {
-        return (a[2] < b[2]);
-    }
     int minimumCost(int n, vector<vector<int>>& connections) {
-        UnionFind u(n+1);
-        sort(begin(connections),end(connections),comp);
-        int res=0;
-        for (int i = 0; i < connections.size(); i++)
+        vector<vector<pair<int,int>>> m(n+1);
+        vector<bool> vis(n+1,false);
+        vector<int> minCost(n+1,INT_MAX);
+        for (auto &&i : connections)
         {
-            if(u.unionn(connections[i][0],connections[i][1])){
-                n-=1;
-                res+=connections[i][2];
-            }
+            m[i[0]].push_back({i[2],i[1]});
+            m[i[1]].push_back({i[2],i[0]});
         }
-        return u.components-1 == 1 ? res : -1;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> q;
+        q.push({0,1});
+        int numvis = 0, res=0;
+        while (numvis<n && !q.empty())
+        {
+            int cost = q.top().first;
+            int city = q.top().second;
+            q.pop();
+            if(vis[city])continue;
+            numvis++;
+            res+=cost;
+            vis[city]=true;
+            for (auto &&i : m[city])
+            {
+                cost = i.first;
+                city = i.second;
+                if(!vis[city] && cost<minCost[city]){
+                    minCost[city] = cost;
+                    q.push({cost,city});
+                } 
+            }
+            
+        }
+        return numvis == n?res:-1;
     }
 };
