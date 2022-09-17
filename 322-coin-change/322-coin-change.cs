82916a0d46d0d2e9dfab64270b9930ae47d00927
Anshuman Padhi <liku1997@gmail.com>
2022-09-17 08:07:01 +0530

    Time: 255 ms (16.89%), Space: 39.6 MB (69.98%) - LeetHub

--- 322-coin-change/322-coin-change.cs
+++ 322-coin-change/322-coin-change.cs
@@ -1,22 +1,21 @@
 public class Solution {
     int[,] dp = new int[10001,14];
-    public int CoinChange(int[] c, int t) {
-        var a = h(c,t,c.Length);
+    public int CoinChange(int[] coins, int t) {
+        Array.Sort(coins);
+        var a = h(coins,t,coins.Length);
         if(a==int.MaxValue-1)return -1;
         return a;
     }
-    public int h(int[] c,int t,int i){
+    public int h(int[] coins,int t,int i){
         if(i==0||t==0){
             if(t==0)return 0;
             return int.MaxValue-1;
         }
-        if(dp[t,i]>0){
-            return dp[t,i];
-        }
-        if(c[i-1]>t){
-            return dp[t,i]=h(c,t,i-1);
+        if(dp[t,i]>0)return dp[t,i];
+        if(coins[i-1]>t){
+            return h(coins,t,i-1);
         }else{
-            return dp[t,i]=Math.Min(h(c,t,i-1),1+h(c,t-c[i-1],i));
+            return dp[t,i]=Math.Min(h(coins,t,i-1),1+h(coins,t-coins[i-1],i));
         }
     }
 }
