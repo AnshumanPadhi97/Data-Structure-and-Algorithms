09b60615266d4f03350c41cf722bd9ed4d0a6a83
Anshuman Padhi <liku1997@gmail.com>
2022-09-18 07:54:10 +0530

    Time: 105 ms (90.62%), Space: 39.8 MB (56.14%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cs
+++ 42-trapping-rain-water/42-trapping-rain-water.cs
@@ -1,21 +1,18 @@
 public class Solution {
-    public int Trap(int[] height) {
-            var st = new Stack<int>();
-            int l = height.Length;
-            var res = 0;
-            for (int i = 0; i < height.Length; i++)
-            {
-                while (st.Count > 0 && height[st.Peek()] < height[i])
-                {
-                    var bottom = height[st.Pop()];
-                    if (st.Count == 0) break;
-
-                    var left = st.Peek();
-                    var w = (i - left - 1) * (Math.Min(height[i], height[left]) - bottom);
-                    res += w;
-                }
-                st.Push(i);
+    public int Trap(int[] h) {
+        var st = new Stack<int>();
+        int l = h.Length;
+        int res=0;
+        for(int i=0;i<l;i++){
+            while(st.Count>0 && h[st.Peek()]<h[i]){
+                int bottom = h[st.Pop()];
+                if(st.Count==0)break;
+                int left = st.Peek();
+                var w = (i-left-1)*(Math.Min(h[left],h[i])-bottom);
+                res+=w;
             }
-            return res;
+            st.Push(i);
+        }
+        return res;
     }
 }
