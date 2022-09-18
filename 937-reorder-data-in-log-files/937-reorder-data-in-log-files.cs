4dc75febe77adae7ecf23e74091027cab9fcb16b
Anshuman Padhi <liku1997@gmail.com>
2022-09-18 19:31:03 +0530

    Time: 176 ms (88.02%), Space: 50.7 MB (5.01%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cs
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cs
@@ -1,31 +1,22 @@
 public class Solution {
-        public string[] ReorderLogFiles(string[] logs)
-        {
-            var d = new List<string>();
-            var l = new List<string>();
-            foreach (var item in logs)
-            {
-                if(char.IsDigit(item.Split(' ')[1][0])){
-                    d.Add(item);
-                }
-                else
-                {
-                    l.Add(item);
-                }
-            }
-            l.Sort(new comp());
-            l.AddRange(d);
-            return l.ToArray();
-        }
-        public class comp : IComparer<string>
-        {
-            public int Compare(string a,string b)
-            {
-                var l1 = a.Split(' ', 2);//identifier, content
-                var l2 = b.Split(' ', 2);
-                var c = l1[1].CompareTo(l2[1]);//compare contents
-                if (c != 0) return c;
-                else return l1[0].CompareTo(l2[0]);//if same content then compare identifier
+    public string[] ReorderLogFiles(string[] logs) {
+        var a = new List<string>();
+        var b = new List<string>();
+        foreach(var item in logs){
+            if(char.IsDigit(item.Split(' ')[1][0])){
+                b.Add(item);
+            }else{
+                a.Add(item);
             }
         }
+        a.Sort((a,b)=>{
+            var l1 = a.Split(' ',2);
+            var l2 = b.Split(' ',2);
+            var c = l1[1].CompareTo(l2[1]);
+            if(c!=0)return c;
+            return l1[0].CompareTo(l2[0]);
+        });
+        a.AddRange(b);
+        return a.ToArray();
+    }
 }
