cd23c726a2057881c623544f4671c38435bdab68
Anshuman Padhi <liku1997@gmail.com>
2022-09-19 09:58:46 +0530

    Time: 335 ms (5.02%), Space: 59.1 MB (63.01%) - LeetHub

---
+++ 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long minimumHealth(vector<int>& damage, int armor) {
+        int maxi = 0;
+        long health=1;
+        for (int i = 0; i < damage.size(); i++)
+        {
+            health+=damage[i];
+            maxi=max(maxi,damage[i]);
+        }
+        health-=min(armor,maxi);
+        return health;
+    }
+};
