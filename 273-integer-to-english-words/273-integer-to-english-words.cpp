11bf25dad868d61baf8dddee6458311444a41058
Anshuman Padhi <liku1997@gmail.com>
2022-09-20 09:29:32 +0530

    Time: 5 ms (56.63%), Space: 6.3 MB (60.96%) - LeetHub

--- 273-integer-to-english-words/273-integer-to-english-words.cpp
+++ 273-integer-to-english-words/273-integer-to-english-words.cpp
@@ -1,19 +1,3 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-#define ll long long
-typedef unsigned long long ull;
-typedef long double lld;
-#define all(v) v.begin(), v.end()
-#define revall(v) v.rbegin(), v.rend()
-#define setbits __builtin_popcountll
-#define nline "\n"
-#define MOD1 (int)998244353
-#define INF64 (ll)1e18
-#define INF32 (int)1e9+7
-#define yes cout<<"YES"
-#define no cout<<"NO"
-
 string digits[20] = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
 string tens[10] = {"Zero", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
 
