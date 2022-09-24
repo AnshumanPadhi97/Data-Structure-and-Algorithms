39640fc507b78a8423234449f3df9e9d7ea550fa
Anshuman Padhi <liku1997@gmail.com>
2022-09-24 07:57:32 +0530

    Time: 316 ms (5.05%), Space: 17.4 MB (37.53%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,26 +1,22 @@
-
 class Solution {
 public:
     int minDeletions(string s) {
-        map<char,int> m;
-        for (int i = 0; i < s.size(); i++)
-        {
-            m[s[i]]++;
+        map<char,int>m;
+        for(auto i:s){
+            m[i]++;
         }
         priority_queue<int> q;
-        for (auto &&i : m)
-        {
-            if(i.second>0)
-                q.push(i.second);
+        for(auto i:m){
+            q.push(i.second);
         }
         int res=0;
-        while (q.size()>1)
-        {
+        while(q.size()>1){
             int a = q.top();
             q.pop();
             if(a==q.top()){
-                if(a-1>0)
+                if(a-1>0){
                     q.push(a-1);
+                }
                 res++;
             }
         }
