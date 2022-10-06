39c52d62ac2bb0a1b3794ed5fe618c2bb2da7cc8
Anshuman Padhi <liku1997@gmail.com>
2022-10-06 10:51:30 +0530

    Time: 744 ms (22.59%), Space: 130.5 MB (36.13%) - LeetHub

---
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -0,0 +1,24 @@
+class TimeMap {
+public:
+    unordered_map<string,map<int,string>> m;
+    
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        m[key][timestamp]=value;
+    }
+    
+    string get(string key, int timestamp) {
+        if(m.find(key)==m.end()){
+            return "";
+        }
+        
+        auto it = m[key].upper_bound(timestamp);
+        
+        if(it==m[key].begin()) return "";
+
+        return prev(it)->second;
+    }
+};
