0d49ee4cbbbb01e3c817439c63555c1035bafc77
Anshuman Padhi <liku1997@gmail.com>
2022-10-07 09:14:42 +0530

    Time: 1339 ms (5.22%), Space: 308.3 MB (5.03%) - LeetHub

--- 732-my-calendar-iii/732-my-calendar-iii.cpp
+++ 732-my-calendar-iii/732-my-calendar-iii.cpp
@@ -1,23 +1,24 @@
 class MyCalendarThree {
-private:
-    map<int, int> diff;
-
 public:
-    MyCalendarThree() {}
-
-    int book(int start, int end) {
-        int cur = 0, res = 0;
-        diff[start]++;
-        diff[end]--;
-        for (auto& [_, delta] : diff) {
-            cur += delta;
-            res = max(res, cur);
+    unordered_map<int,int> vals;
+    unordered_map<int,int> lazy;
+    MyCalendarThree() {
+        
+    }
+    void update(int start,int end,int left,int right,int idx){
+        if(start>right || end<left) return;
+        if(left >= start && right <= end){
+            lazy[idx]++;
+            vals[idx]++;
+        }else{
+            int mid = (left+right)>>1;
+            update(start,end,left,mid,idx*2);
+            update(start,end,mid+1,right,idx*2+1);
+            vals[idx]=lazy[idx]+max(vals[idx*2],vals[idx*2+1]);
         }
-        return res;
+    }
+    int book(int start, int end) {
+        update(start,end-1,1,1000000000,1);
+        return vals[1];
     }
 };
-/**
- * Your MyCalendarThree object will be instantiated and called as such:
- * MyCalendarThree* obj = new MyCalendarThree();
- * int param_1 = obj->book(start,end);
- */
