f333acfd24e0c1f87ff48846e4b60e9fb96619be
Anshuman Padhi <liku1997@gmail.com>
2022-10-09 10:07:23 +0530

    Time: 1442 ms (100.00%), Space: 60.7 MB (100.00%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cs
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cs
@@ -1,29 +1,48 @@
 public class Solution {
-    
-        int mod = (int)(Math.Pow(10, 9) + 7);
+    int mod = (int)(Math.Pow(10, 9) + 7);
         int res = 0;
         public int dfs(int i, int j, int[][] grid, int k, int curr, int[,,] dp)
         {
-            if (i == grid.Length || j == grid[0].Length)
+            if (i < 0 || j < 0 || i >= grid.Length || j >= grid[0].Length)
             {
                 return 0;
-            }
-            if (i == grid.Length-1 && j == grid[0].Length-1)
-            {
-                curr += grid[i][j];
-                return curr % k == 0 ? 1 : 0;
             }
             if (dp[i, j, curr % k] > 0)
             {
                 return dp[i, j, curr % k];
             }
-            int a = dfs(i + 1, j, grid, k, curr + grid[i][j], dp) + dfs(i, j + 1, grid, k, curr + grid[i][j], dp);
+            if (i == 0 && j == 0)
+            {
+                curr+=grid[0][0]%k;
+                return curr%k==0?1:0;
+            }
+            int a = dfs(i - 1, j, grid, k, curr + grid[i][j], dp) + dfs(i, j - 1, grid, k, curr + grid[i][j], dp);
             return dp[i, j, curr % k] = a % mod;
         }
         public int NumberOfPaths(int[][] grid, int k)
-        {   
-            var dp = new int[grid.Length + 1, grid[0].Length + 1, k + 1];
-            res = dfs(0, 0, grid, k, 0, dp);
+        {
+            res = 0;
+            if (grid.Length == 1 || grid[0].Length == 1)
+            {
+                var sum = 0;
+                for (int i = 0; i < grid.Length; i++)
+                {
+                    for (int j = 0; j < grid[0].Length; j++)
+                    {
+                        sum += grid[i][j];
+                    }
+                }
+                if (sum % k == 0)
+                {
+                    return 1;
+                }
+                return 0;
+            }
+
+            int m = grid.Length, n = grid[0].Length;
+
+            var dp = new int[grid.Length, grid[0].Length, k];
+            res = dfs(m-1, n-1, grid, k, 0, dp);
             return res % mod;
         }
 }
