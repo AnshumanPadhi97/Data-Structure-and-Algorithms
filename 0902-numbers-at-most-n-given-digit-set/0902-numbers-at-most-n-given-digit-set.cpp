e1900ba031a5056dd40d08914e14608068d410ec
Anshuman Padhi <liku1997@gmail.com>
2022-10-11 14:53:07 +0530

    Time: 8 ms (9.02%), Space: 8.2 MB (44.06%) - LeetHub

---
+++ 0902-numbers-at-most-n-given-digit-set/0902-numbers-at-most-n-given-digit-set.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int atMostNGivenDigitSet(vector<string>& digits, int n) {
+        string a = to_string(n);
+        int l = a.size(), dsize = digits.size();
+        int res=0;
+        
+        for(int i=1;i<l;i++){//no of numbers from 1 to n-1 len which are small than n
+            res+=pow(dsize,i);
+        }
+        
+        for(int i=0;i<l;i++){
+            bool hasSameNum = false;
+            for(auto s:digits){
+                if(s[0]<a[i]){
+                    res+=pow(dsize,l-i-1);
+                }
+                else if(s[0]==a[i]){
+                    hasSameNum=true;
+                }
+            }
+            if(!hasSameNum) return res;
+        }
+        return res+1;
+    }
+};
