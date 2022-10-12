27845c57e64bdae5991f3f5d8a6119dc7764f35d
Anshuman Padhi <liku1997@gmail.com>
2022-10-12 10:07:05 +0530

    Time: 244 ms (52.66%), Space: 60.7 MB (51.63%) - LeetHub

--- 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
@@ -1,58 +1,55 @@
 class Solution {
 public:
+
     vector<int> topo(vector<vector<int>>& g,int k){
-        vector<int> cnt(k+1,0);
-        vector<int> adj[k+1];
         vector<int> res;
         queue<int> q;
-        for (auto &&i : g)
-        {
-            cnt[i[1]]++;
+        vector<int> indegree(k+1,0);
+        vector<int> adj[k+1];
+        for(auto i:g){
+            indegree[i[1]]++;
             adj[i[0]].push_back(i[1]);
         }
         for (int i = 1; i <= k; i++)
         {
-            if(cnt[i]==0){
+            if(indegree[i]==0){
                 q.push(i);
             }
         }
-
         while (!q.empty())
         {
-            int t = q.front();
+            int t=q.front();
             q.pop();
             res.push_back(t);
-            for (auto &&i : adj[t])
-            {
-                cnt[i]--;
-                if(cnt[i]==0){
+            for(auto i:adj[t]){
+                indegree[i]--;
+                if(indegree[i]==0){
                     q.push(i);
                 }
             }
-        }
+        }        
         return res;
-}
+    }
+    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
+        auto topo1 = topo(rowConditions,k);
+        if(topo1.size()!=k) return {};
 
-vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
-        vector<vector<int>> res(k,vector<int> (k,0));
-
-        vector<int> row = topo(rowConditions,k);
-        if(row.size()!=k)return {};
-
-        vector<int> col = topo(colConditions,k);
-        if(col.size()!=k)return {};
-
-        vector<int> idx(k+1,0);
-        for (int j = 0; j < col.size(); j++)
+        auto topo2 = topo(colConditions,k);
+        if(topo2.size()!=k) return {};
+        
+        vector<vector<int>> res(k,vector<int>(k,0));
+        
+        vector<int> ind(k+1,0);
+        for (int i = 0; i < topo2.size(); i++)
         {
-            idx[col[j]]=j;
+            ind[topo2[i]]=i;
         }
-
+        
         for (int i = 0; i < k; i++)
         {
-            res[i][idx[row[i]]]=row[i];
-        }		
-
+            res[i][ind[topo1[i]]]=topo1[i];   
+        }        
+        
         return res;
     }
 };
